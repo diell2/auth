168465dea114d7cfbb526a38e6b6a3058f534356
diell2 <ziellzustafa@gmail.com>
2025-01-29 08:48:41 -0800

    new

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 
-const Login=()=>{
+const Login=({navigation})=>{
     const [username, setUsername]=useState("")
     const [password, setPassword]=useState("")
     const Handle= async()=>{
@@ -9,14 +9,19 @@ const Login=()=>{
             const pergjigjja= await fetch("https://dummyjson.com/auth/login",{
                 method:"POST",
                 headers:{'Content-Type':"application/json"},
-                body:JSON.stingify({
+                body:JSON.stringify({
                     username:username,
                     password:password
                 }),
                 credentials:"omit"
             })
             const data= await pergjigjja.json()
-            console.log(data)
+            if(data?.accessToken){
+                navigation.navigate("Welcome",{
+                    username:username
+                })
+            }
+
         }
         catch(err){
 
---
+++ screens/newpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native-web";
+
+const newpage = ({route}) => {
+    <View>
+        <Text>This is next page</Text>
+    </View>
+    const {}=route.params || {}
+        return(<View>
+            <Text>This is next page</Text>
+        </View>)
+}
+
+export default newpage;
